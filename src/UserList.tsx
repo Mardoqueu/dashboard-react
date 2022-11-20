8425c2f7005fe8673efd57f23012529ee72a9b07
Mardoqueu Sousa <mdq767@gmail.com>
2022-11-20 16:42:14 -0300

    Customizing Styles

--- src/UserList.tsx
+++ src/UserList.tsx
@@ -1,7 +1,7 @@
 // in src/users.tsx
 import { useMediaQuery } from "@mui/material";
-import { List, SimpleList, Datagrid, TextField, EmailField, UrlField } from "react-admin";
-
+import { List, SimpleList, Datagrid, TextField, EmailField } from "react-admin";
+import MyUrlField from './MyUrlField';
 export const UserList = () => {
   const isSmall = useMediaQuery((theme) => theme.breakpoints.down("sm"));
   return (
@@ -20,7 +20,7 @@ export const UserList = () => {
           <EmailField source="email" />
           <TextField source="address.street" />
           <TextField source="phone" />
-          <UrlField source="website" />
+          <MyUrlField  source="website" />
           <TextField source="company.name" />
         </Datagrid>
       )}
