bbd17fd9b633a0b74a8e85ad7e85de80fcc6c4e7
Mardoqueu Sousa <mdq767@gmail.com>
2022-11-20 18:27:04 -0300

    Using a Custom Home Page and Customizing the Menu Icons

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,17 @@ import jsonServerProvider from "ra-data-json-server";
 import { UserList } from "./UserList";
 import { PostList, PostEdit, PostCreate } from "./ListGuesser";
 
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
+
+import { Dashboard } from './Dashboard';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
- <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
-    <Resource name="users" list={UserList} recordRepresentation="name" />
-
-   </Admin>
+  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+    <Resource name="users" list={UserList} icon={UserIcon} recordRepresentation="name" />
+  </Admin>
   );
 export default App;
